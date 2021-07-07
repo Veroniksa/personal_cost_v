cef9ca15138547b93f0ba64328d4ef3085b4cbe5
Veronika <desatericenkoveronika@gmail.com>
2021-07-07 07:23:36 +0200

    show = !show

--- personal_cost/src/components/AddPayment.vue
+++ personal_cost/src/components/AddPayment.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
+        <div>
+            <button @click="toggle">ADD NEW COST +</button>
+        </div>
+        <div v-show="show">
         <input v-model="date" placeholder="date"/>
         <input v-model="category" placeholder="category"/>
         <input v-model.number="value" type="number" placeholder="value"/>
         <button @click="onClick">
             Add Data
         </button>
+    </div>
+    
     </div>
 </template>
 
@@ -16,30 +22,34 @@ export default {
       return {
           date: '',
           category: '',
-          value: null
+          value: null,
+          show: false
       }
   },
-  methods: {
-      onClick(){
-          const { category, value } = this
-        const data = {
-            date: this.date || this.getCurrentDate,
-            category,
-            value
+methods: {
+    toggle () {
+        this.show = !this.show
+    },
+        onClick(){
+            const { category, value } = this
+            const data = {
+                date: this.date || this.getCurrentDate,
+                category,
+                value
+            }
+            console.log('add', data)
+            //Вызов события, название события и аргументы
+            this.$emit('addNewPayment', data)
         }
-        console.log('add', data)
-        //Вызов собятия, название собятия и аргументы
-        this.$emit('addNewPayment', data)
-      }
-  },
-  comuted: {
-      getCurrentDate() {
-          const today = new Date()
-          const d = today.getDate()
-          const m = today.getMonth() + 1
-          const y = today.getFullYear()
-          return `${d}.${m}.${y}`
-      }
-  }
+    },
+    computed: {
+        getCurrentDate() {
+            const today = new Date()
+            const d = today.getDate()
+            const m = today.getMonth() + 1
+            const y = today.getFullYear()
+            return `${d}.${m}.${y}`
+        }
+    }
 }
 </script>
