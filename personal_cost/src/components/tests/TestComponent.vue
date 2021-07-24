9503b085a9b0c3f95d05329de62b2c83ac94a7b4
Veronika <desatericenkoveronika@gmail.com>
2021-07-24 18:45:14 +0200

    les7/test

---
+++ personal_cost/src/components/tests/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        The message is: {{ message }} (modified)
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        message: String
+    }
+}
+</script>
---
+++ personal_cost/src/components/tests/testComponent.test.js
@@ -0,0 +1,38 @@
+import {mount} from '@vue/test-utils';
+import TestComponent from './TestComponent';
+
+describe('TestComponent', ()=> {
+    test("Test Content Component", ()=> {
+        const wrapper = mount(TestComponent, {
+            propsData: {
+                message: "Hello from tests!"
+            }
+        });
+
+        expect(wrapper.text()).toEqual(
+            'The message is: Hello from tests!'
+            );
+    });
+    test("Test Content html", ()=> {
+        const wrapper = mount(TestComponent, {
+            propsData: {
+                message: "Hello from tests!"
+            }
+        });
+
+        expect(wrapper.html()).toEqual(
+            'The message is: Hello from tests!'
+            );
+    });
+        test("Test Content toContein", ()=> {
+        const wrapper = mount(TestComponent, {
+            propsData: {
+                message: "Hello from tests!"
+            }
+        });
+
+        expect(wrapper.text()).toContain(
+            'The message is: Hello from tests!'
+            );
+    });
+});
