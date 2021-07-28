5fb6c51d06e509e27e8dcb79b853fdb9aef34e2c
Veronika <desatericenkoveronika@gmail.com>
2021-07-28 20:53:35 +0200

    chenge

--- personal_cost/src/App.vue
+++ personal_cost/src/App.vue
@@ -5,31 +5,15 @@
     </header>
     <div class="menu">
       <router-link to="/dashboard">Dashboard</router-link> /
-      <router-link to="/about">About</router-link> /
-      <button @click="goToThePageNoFound">Not Found</button>
-      <!--       <router-link to="/NotFound">Not Found</router-link>
- --><!--       <a href="dashboard">Dashboard</a> /
-      <a href="about">About</a> /
-      <a href="notfound">Not Found</a> -->
+      <router-link to="/about">About</router-link>
     </div>
     <main>
       <div class="content-page">
         <router-view></router-view>
-        <!--         <About v-if="page === 'about'"/>
-        <Dashboard v-if="page === 'dashboard'"/>
-        <NotFound v-if="page === 'notfound'"/> -->
       </div>
-
-      <!-- <AddPayment @addNewPayment="addData" /> -->
       <br />
-      Totatl: {{ getFPV }}
-      <PaymentsDisplay :list="currentElements" />
-      <Pagination
-        :cur="curPage"
-        :n="n"
-        :length="paymentsList.length"
-        @paginate="onChangePage"
-      />
+
+      
       <transition name="fade">
         <modal-window
           @close="onClose"
@@ -38,8 +22,7 @@
           :undateSettings="undateSettings"
         />
       </transition>
-      <button @click="showPaymenysForm">Show Paymenys Form</button>
-      <button @click="closePaymenysForm">Close Paymenys Form</button>
+
       <transition name="fade">
         <modal-context-menu
           :setting="undateSettings"
@@ -49,9 +32,7 @@
             top: `${this.undateSettings.y + 5}px`,
             left: `${this.undateSettings.x - 80}px`,
           }"
-        /><!-- :setting="undateSettings"
-        v-if="undateSettings.name"      // v-if="undateSettings.name"
-        :settings="undateSettings"-->
+        />
       </transition>
     </main>
   </div>
@@ -59,28 +40,10 @@
 
 
 <script>
-import PaymentsDisplay from "./components/PaymentsDisplay.vue";
-// import AddPayment from "./components/AddPayment.vue";
-import Pagination from "./components/Pagination.vue";
-/*import CategorySelect from "./components/CategorySelect.vue"
-<CategorySelect :categories="categories" />*/
-
-/* import Dashboard from './views/Dashboard.vue'
-import About from './views/About.vue'
-import NotFound from './views/NotFound.vue' */
-
-import { mapMutations, mapGetters, mapActions } from "vuex";
 import ModalContextMenu from "./components/ModalContextMenu.vue";
-
 export default {
   name: "App",
   components: {
-    PaymentsDisplay,
-    Pagination,
-    /*     Dashboard,
-    About,
-    NotFound, */
-    // AddPayment,
     ModalWindow: () =>
       import(
         /* webpackChunkName: 'ModalWindow'*/ "./components/ModalWindow.vue"
@@ -89,49 +52,16 @@ export default {
   },
   data() {
     return {
-      page: "",
       settings: {
         header: "AddPayment",
         content: "addPaymentForm",
       },
-      curPage: 1,
-      n: 3,
       modalShown: false,
       modalSettings: {},
       undateSettings: {},
     };
   },
   methods: {
-    ...mapMutations([
-      "setPaymentsListData",
-      "addDataToPaymentsList",
-      "deletPaymentListData",
-    ]),
-    ...mapActions(["fetchData", "fetchCategory"]),
-    addData(data) {
-      //console.log(data)
-      // this.paymentsList.push(data)
-      //this.paymentsList = [...this.paymentsList, data]
-      this.addDataToPaymentsList(data);
-    },
-    goToThePageNoFound() {
-      this.$router.push({ name: "NotFound" });
-    },
-    onClose() {
-      this.addFormShown = false;
-    },
-    /*     setPage(){
-      this.page = location.pathname.slice(1)
-    } */
-    onChangePage(p) {
-      this.curPage = p;
-    },
-    showPaymenysForm() {
-      this.$modal.show("AddPayment", { header: "Add" });
-    },
-    closePaymenysForm() {
-      this.$modal.hide();
-    },
     onShown(settings) {
       this.modalSettings = settings;
       //console.log(settings)
@@ -146,34 +76,9 @@ export default {
       this.undateSettings = {};
     },
   },
-  computed: {
-    ...mapGetters({
-      paymentsList: "getPaymentList",
-      categories: "getCategoryList",
-    }),
-    currentElements() {
-      const { n, curPage } = this;
-      return this.paymentsList.slice(n * (curPage - 1), n * (curPage - 1) + n);
-    },
-    getFPV() {
-      return this.$store.getters.getFuulPaymentValue;
-    },
-    /*     paymentsList(){
-      return this.$store.getters.getPaymentList
-    } */
-  },
-  created() {
-    //this.$store.commit('setPaymentsListData', this.fetchData()) //mutations
-    //this.setPaymentsListData(this.fetchData())
-    //this.$store.dispatch('fetchData') //actions(нативная запись)
-    this.fetchData();
-    if (!this.categories.length) {
-      this.fetchCategory();
-    }
-  },
   mounted() {
-    const page = this.$route.params.page || 1;
-    this.curPage = Number(page);
+/*     const page = this.$route.params.page || 1;
+    this.curPage = Number(page); */
     this.$modal.EventBus.$on("shown", this.onShown);
     this.$modal.EventBus.$on("hide", this.onHide);
     this.$modale.EventBus.$on("showMenu", this.onItemsShow);
@@ -204,7 +109,6 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-
   header {
     background: linear-gradient(45deg, white, transparent);
   }
--- personal_cost/src/views/Dashboard.vue
+++ personal_cost/src/views/Dashboard.vue
@@ -1,9 +1,96 @@
 <template>
-  <div>Dashboard</div>
+  <div>Dashboard
+          Totatl: {{ getFPV }}
+      <PaymentsDisplay :list="currentElements" />
+      <Pagination
+        :cur="curPage"
+        :n="n"
+        :length="paymentsList.length"
+        @paginate="onChangePage"
+      />
+            <button @click="showPaymenysForm">Show Paymenys Form</button>
+      <button @click="closePaymenysForm">Close Paymenys Form</button>
+  </div>
 </template>
 
 <script>
+import { mapMutations, mapGetters, mapActions } from "vuex";
+import PaymentsDisplay from "../components/PaymentsDisplay.vue";
+import Pagination from "../components/Pagination.vue";
   export default {
-      name: "Dashboard"
+      name: "Dashboard",
+      components: {
+      PaymentsDisplay,
+      Pagination,
+    },
+      data() {
+    return {
+      page: "",
+      settings: {
+        header: "AddPayment",
+        content: "addPaymentForm",
+      },
+      curPage: 1,
+      n: 3,
+      modalShown: false,
+      modalSettings: {},
+      undateSettings: {},
+    };
+  },
+    methods: {
+    ...mapMutations([
+      "setPaymentsListData",
+      "addDataToPaymentsList",
+      "deletPaymentListData",
+    ]),
+    ...mapMutations([
+      "setPaymentsListData",
+      "addDataToPaymentsList",
+      "deletPaymentListData",
+    ]),
+    ...mapActions(["fetchData", "fetchCategory"]),
+    addData(data) {
+      this.addDataToPaymentsList(data);
+    },
+    goToThePageNoFound() {
+      this.$router.push({ name: "NotFound" });
+    },
+    onClose() {
+      this.addFormShown = false;
+    },
+    onChangePage(p) {
+      this.curPage = p;
+    },
+    showPaymenysForm() {
+      this.$modal.show("AddPayment", { header: "Add" });
+    },
+    closePaymenysForm() {
+      this.$modal.hide();
+    },
+  },
+    computed: {
+    ...mapGetters({
+      paymentsList: "getPaymentList",
+      categories: "getCategoryList",
+    }),
+    currentElements() {
+      const { n, curPage } = this;
+      return this.paymentsList.slice(n * (curPage - 1), n * (curPage - 1) + n);
+    },
+    getFPV() {
+      return this.$store.getters.getFuulPaymentValue;
+    },
+
+  },
+    created() {
+    this.fetchData();
+    if (!this.categories.length) {
+      this.fetchCategory();
+    }
+  },
+    mounted() {
+     const page = this.$route.params.page || 1;
+    this.curPage = Number(page);
+    }
   }
 </script>
